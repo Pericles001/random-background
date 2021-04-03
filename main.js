1de04437b0e25245971a440c03b49df0c59091e0
Pericles001 <periclesadjovi@utlook.fr>
2021-04-03 23:25:41 +0100

    used random method in js to generate random color code

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const bgEl = document.querySelector('section');
 
 button.addEventListener('click', ()=>{
     let color= '#';
-const random = Math.random();
+
+color+= Math.random().toString(16).slice(2,8);
+
+bgEl.style.backgroundColor=color;
 
 })
