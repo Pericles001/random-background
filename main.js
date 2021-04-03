73dea08d2487a6ad47f818ed223d0e177fe16050
Pericles001 <periclesadjovi@utlook.fr>
2021-04-03 22:29:18 +0100

    defined html elements and started styling them

---
+++ main.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button');
+const h2El = document.querySelector('h2');
+const bgEl = document.querySelector('section');
+
+button.addEventListener('click', ()=>{
+    let color= '#';
+const random = Math.random();
+
+})
